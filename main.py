effe9461d16f1db25809bcaeb779214f7351ec38
Moomerator46 <97137823+Moomerator46@users.noreply.github.com>
2024-09-20 19:26:12 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter
+from tkinter import ttk
+from tkinter import messagebox
+import random
+
+choices = ['Yes', 'No']
+
+window = tk.Tk()
+window.title("Yes/No Randomizer")
+window.geometry('400x300')
+
+def choose():
+  chosen = random.choice(choices)
+  messagebox.showinfo("The choice was...", chosen+"!")
+
+btn = ttk.Button(master=window, text="Get a Y/N answer", command=choose)
+btn.pack()
+
+window.mainloop()
